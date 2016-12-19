417b0a0a03e490b791e045683bd7626fde0d4706
viniciusofp <viniciusofp@gmail.com>
2016-12-19 15:23:14 -0200

    v0.8

--- javascripts/myBlog.js
+++ javascripts/myBlog.js
@@ -1,6 +1,6 @@
 var app = angular.module('jogoDoOrcamento', []);
 
-app.controller('GameCtrl', ['$scope', function($scope){
+app.controller('GameCtrl', ['$scope', '$filter', function($scope, $filter){
 
     $scope.items = [ 
     {
@@ -9,67 +9,67 @@ app.controller('GameCtrl', ['$scope', function($scope){
         color: '#3EBB47'
     }, 
     {
-    	value: 2,
+    	value: 1,
     	name: 'Judiciária',
         color: '#3E96BB'
     }, 
     {
-    	value: 3,
+    	value: 1,
     	name: 'Essencial à Justiça',
         color: '#BB4D3E'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Administração',
         color: '#efe63d'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Defesa Nacional',
         color: '#3EBBAB'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Segurança Pública',
         color: '#2A7E38'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Relações Exteriores',
         color: '#BB3E89'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Assistência Social',
         color: '#62CBC4'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Previdência Social',
         color: '#0F2E1F'
     }, 
     {
-    	value: 4,
+    	value: 15,
     	name: 'Saúde',
         color: '#BF406A'
     }, 
     {
-    	value: 4,
+    	value: 2,
     	name: 'Trabalho',
         color: '#2B827F'
     }, 
     {
-    	value: 4,
+    	value: 25,
     	name: 'Educação',
         color: '#E0E2A7'
     }, 
     {
-    	value: 4,
+    	value: 6,
     	name: 'Cultura',
         color: '#7E2A5D'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Direitos da Cidadania',
         color: '#C9EDE7'
     }, 
@@ -79,12 +79,12 @@ app.controller('GameCtrl', ['$scope', function($scope){
         color: '#6B3924'
     }, 
     {
-    	value: 4,
+    	value: 2,
     	name: 'Habitação',
         color: '#D279CB'
     }, 
     {
-    	value: 4,
+    	value: 2,
     	name: 'Saneamento',
         color: '#184849'
     }, 
@@ -114,12 +114,12 @@ app.controller('GameCtrl', ['$scope', function($scope){
         color: '#CCCC66'
     }, 
     {
-    	value: 4,
+    	value: 2,
     	name: 'Comércio e Serviços',
         color: '#B6E7D1'
     }, 
     {
-    	value: 4,
+    	value: 2,
     	name: 'Comunicações',
         color: '#181339'
     }, 
@@ -129,7 +129,7 @@ app.controller('GameCtrl', ['$scope', function($scope){
         color: '#BB7B3E'
     }, 
     {
-    	value: 4,
+    	value: 1,
     	name: 'Transporte',
         color: '#225467'
     }, 
@@ -139,12 +139,12 @@ app.controller('GameCtrl', ['$scope', function($scope){
         color: '#C3514B'
     }, 
     {
-    	value: 4,
+    	value: 0.9,
     	name: 'Encargos Especiais',
         color: '#D5F1DC'
     }, 
     {
-    	value: 4,
+    	value: 0.1,
     	name: 'Reserva de Contingência',
         color: '#321123'
     } ];
@@ -153,6 +153,60 @@ app.controller('GameCtrl', ['$scope', function($scope){
     // $scope.slider = function() {
         
     // };
+    $scope.orcTotal = 3000000000000;
+
+    $scope.valueTotal = function(val) {
+
+        var somaTotal = val;
+        if (somaTotal > 2000000000000) {
+            somaTotal = somaTotal / 1000000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' trilhões';
+        } else if (somaTotal > 1000000000000) {
+            somaTotal = somaTotal / 1000000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' trilhão';
+        } else if (somaTotal > 2000000000) {
+            somaTotal = somaTotal / 1000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' bilhões';
+        } else if (somaTotal > 1000000000) {
+            somaTotal = somaTotal / 1000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' bilhão';
+        } else if (somaTotal > 2000000) {
+            somaTotal = somaTotal / 1000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' milhões';
+        }else if (somaTotal > 1000000) {
+            somaTotal = somaTotal / 1000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' milhão';
+        } else if (somaTotal > 1000) {
+            somaTotal = somaTotal / 1000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' mil';
+        };
+    }
+
+    $scope.valueItem = function (item){
+        var somaTotal = $scope.orcTotal / 100 * item.value;
+        if (somaTotal > 2000000000000) {
+            somaTotal = somaTotal / 1000000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' trilhões';
+        } else if (somaTotal > 1000000000000) {
+            somaTotal = somaTotal / 1000000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' trilhão';
+        } else if (somaTotal > 2000000000) {
+            somaTotal = somaTotal / 1000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' bilhões';
+        } else if (somaTotal > 1000000000) {
+            somaTotal = somaTotal / 1000000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' bilhão';
+        } else if (somaTotal > 2000000) {
+            somaTotal = somaTotal / 1000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' milhões';
+        }else if (somaTotal > 1000000) {
+            somaTotal = somaTotal / 1000000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' milhão';
+        } else if (somaTotal > 1000) {
+            somaTotal = somaTotal / 1000;
+            return $filter('currency')(somaTotal, 'R$', 2) + ' mil';
+        };
+    }
 
     $scope.sum = function(list) {
         var subtotal=0;
@@ -162,6 +216,15 @@ app.controller('GameCtrl', ['$scope', function($scope){
      	var total = 100 - subtotal
         return total.toFixed(1);
     }
+
+    $scope.sumLeft = function(soma) {
+        if (soma < 0) {
+            return 'color: red'
+        } else {
+            return 'color: green'
+        };
+    }
+
 
     $scope.style = function(item) {
      	var soma=0;
